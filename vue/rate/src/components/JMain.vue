3012783280d3b7582e75ef8470145f5100b865c8
kjh1113_0 <2104210776@qq.com>
2023-12-03 12:53:15 +0800

    vue

---
+++ vue/rate/src/components/JMain.vue
@@ -0,0 +1,32 @@
+<!-- View -->
+<template>
+    <div>
+        JMain
+        <div v-for="user in users">
+            <Item :user="user" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref }  from 'vue'
+import Item from './Item.vue';
+// 提供数据， 模型
+// Model
+let users = ref(['路修远', '沸羊羊'])
+setTimeout(() => {
+    users.value = [...users.value, 'Yiko']
+}, 2000)
+
+fetch('https://k9gtiz.laf.run/get-list')
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
+        users.value = data.data.map(item => item.text)
+    })
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ yun/laf/auth/readme.md
@@ -0,0 +1,7 @@
+# Laf 授权
+
+- 用户鉴权
+    - register
+        - POST body username password
+        - 201  返回状态码，成功注册 插入users  record
+        - username check    isExisted
