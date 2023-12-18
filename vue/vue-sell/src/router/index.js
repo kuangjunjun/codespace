1c37d24936e7e0c3d6cee36968c42f69663b4c7e
kuangjunjun <kuangjunjun6@gmail.com>
2023-12-18 16:03:10 +0800

    贪心

--- vue/vue-sell/src/router/index.js
+++ vue/vue-sell/src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
   },
   {
     path: '/goods',
+    
     name: 'goods',
     component: () => import('@/views/goods')
   },
--- vue/vue-sell1/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import { get } from './axios.js'
-
-const getSeller = get('/api/seller')
-const getGoods = get('/api/goods')
-
-export {
-  getSeller,
-  getGoods
-}
