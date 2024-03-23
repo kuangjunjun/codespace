d1177925a58907b7344471f79fe232e6f49359d2
@kjh1113_0 <2104210776@qq.com>
2024-03-23 17:19:43 +0800

    http

---
+++ 面试题/xss/app.js
@@ -0,0 +1,21 @@
+// npm init -y
+//npm  i express ejs
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const ejs = require('ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+
+app.get('/', function (req, res) {
+  res.render('index', {title:'express', xss: req.query.xss})
+})
+
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+})
