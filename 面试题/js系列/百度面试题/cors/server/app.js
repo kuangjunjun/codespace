2d8cfbbee77ccc9f94a1a75185136702036c4d65
@kjh1113_0 <2104210776@qq.com>
2024-05-11 18:04:41 +0800

    跨域

--- 面试题/js系列/百度面试题/cors/server/app.js
+++ 面试题/js系列/百度面试题/cors/server/app.js
@@ -1,6 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  // 跨域是浏览器不接受后端的响应
+  // 想个办法，让浏览器不得接受
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*'  // 白名单
+    // 'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'  // 白名单
+  })
+
   let data = {
     msg: "hello cors"
   }
