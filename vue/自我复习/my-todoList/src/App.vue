fce287253a9dae5589a94346ef26d3b78fd7cba2
@kjh1113_0 <2104210776@qq.com>
2024-06-17 23:44:40 +0800

    复习

---
+++ vue/自我复习/my-todoList/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <h2 class="title">todoList</h2>
+    <div class="out">
+      <input type="text" placeholder="请输入待办事项" v-model="content">
+      <button @click="add">添加</button>
+    </div>
+
+    <div class="content">
+      <div v-for="(item, index) in todoList" :key="index">
+        {{ item }}
+        <button @click="del(index)">x</button>        
+      </div>
+    </div>
+
+    
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const todoList = ref(['html', 'css'])
+const content = ref('')
+
+const add = () => {
+  // console.log(content.value);
+  todoList.value.push(content.value)
+}
+
+const del = (index) => {
+  todoList.value.splice(index, 1)
+}
+
+</script>
+
+<style lang="less" scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+.container{
+  .title{
+    text-align: center;
+  }
+  .out{
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    input{
+      width: 300px;
+    }
+    button{
+      width: 100px;
+    }
+  }
+  .content{
+    margin-top: 20px;
+
+  }
+}
+</style>
--- 面试题/大文件上传1/server/app.js
+++ 面试题/大文件上传1/server/app.js
@@ -1,15 +1,72 @@
 const http = require('http')
+const multiparty = require('multiparty');  // 用于解析前端传过来的formData数据
+const path = require('path');
+const fse = require('fs-extra')
 
-const server = http.createServer((req, res) => {
-    // 解决跨域
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', '*')
+const UPLOAD_DIR = path.resolve(__dirname, '.', 'chunks') 
 
-    if (req.url === '/upload') {
-        res.end('hello world')
-    }
+const resolvePost = (req) => {
+    return new Promise((resolve, reject) => {
+        let chunk = ''
+        req.on('data', (data) => {
+            chunk += data
+        })
+        req.on('end', () => {
+            resolve(JSON.parse(chunk))
+        })
+    })
+}
+
+const server = http.createServer(async(req, res) => {
+  // 解决跨域
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  if (req.method === 'OPTIONS') {
+    res.status = 200
+    res.end()
+    return
+  }
+  
+
+  // 前端传过来的切片
+  if (req.url === '/upload') { 
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+      res.writeHead(200, { 'content-type': 'text/plain' });
+      res.write('received upload:\n\n');
+      if (err) {
+        console.log(err);
+        return
+      } 
+
+      // console.log(fields, files);
+      const file = files.file[0]  // 切片的内容
+      const fileName = fields.fileName[0]
+      const chunkName = fields.chunkName[0]
+
+      // 将切片存起来
+      const chunkDir = path.resolve(UPLOAD_DIR, `${fileName}-chunks`)
+      console.log(chunkDir);
+
+      if (!fse.existsSync(chunkDir)) {
+        fse.mkdirsSync(chunkDir)
+      }
+      // 将切片写入到文件夹中
+      fse.moveSync(file.path, `${chunkDir}/${chunkName}`)
+
+      res.end('切片上传成功')
+
+    })
+  } else if (req.url === '/merge') {
+      // 合并切片
+      const { fileName } = await resolvePost(req)
+      const file
+  }
+
 })
 
+
+
 server.listen(3000, () => {
-    console.log('server is running at port 3000')
+  console.log('listening on port 3000');
 })
